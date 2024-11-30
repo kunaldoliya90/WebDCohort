18b6418eeed43b72346498afdc35ae2b102613f9
kunaldoliya90 <kunal.doliya90@gmail.com>
2024-11-30 19:54:49 +0530

    I have started week 9

---
+++ Week-9/starting_w_react/src/App.jsx
@@ -0,0 +1,48 @@
+import './App.css'
+
+function App() {
+  return (<div style={{ justifyItems: 'center', display: 'flex', flexDirection: 'column', gap: 20, alignItems: 'center', backgroundColor: 'lightyellow'}} >
+    <ProfileCard
+      profilePhotoUrl={"https://media.licdn.com/dms/image/v2/D4D03AQFPRTa62lvPVw/profile-displayphoto-shrink_200_200/profile-displayphoto-shrink_200_200/0/1730780289099?e=2147483647&v=beta&t=bhj8xdUKlDmjwRmnjQl_srgUp0G5QNlHY9FVlv-HJfQ"}
+      usersName={"Kunal Doliya"}
+      subTitle={"SDE @Microsoft | about kunal doliya's profile"}
+      time={"18m"}
+      postContent={"Stellar Achievements: Grabbed a super dream offer..."}
+      postImageUrl={'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTABbXr4i-QODqhy7tofHYmTYh05rYPktzacw&s'}
+    />
+
+<ProfileCard
+      profilePhotoUrl={"https://cdn-icons-png.flaticon.com/512/174/174857.png"}
+      usersName={"LinkedIn"}
+      subTitle={"Promoted"}
+      // time={"18m"}
+      postContent={"Stellar Achievements: Grabbed a super dream offer..."}
+      postImageUrl={'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTABbXr4i-QODqhy7tofHYmTYh05rYPktzacw&s'}
+    />
+  </div>)
+}
+
+const ProfileCardStyle = { height: 30, width: 30, borderRadius: 20 }
+const imageStyle = { height: '65%', width: '100%' }
+function ProfileCard({ profilePhotoUrl, usersName, subTitle, time, postContent, postImageUrl }) {
+  return (
+    <div style={{ backgroundColor: 'lightblue', width: '250px', height: '200px', borderRadius: 10 }}>
+      <div style={{ display: 'flex', padding: 8 }} >
+        <img
+          src={profilePhotoUrl}
+          alt="HTML5"
+          style={ProfileCardStyle} />
+        <div style={{ fontSize: 8, textAlign: 'left', paddingLeft: '10px' }} >
+          <div>{usersName}</div>
+          <div>{subTitle}</div>
+          {time && <div>{time}</div>}
+        </div>
+      </div>
+      <div style={{ fontSize: 10, textAlign: 'left' }} >
+        {postContent}
+      </div>
+      <img src={postImageUrl} style={imageStyle} />
+    </div>)
+}
+
+export default App
--- Week10/learningreact/src/App.jsx
+++ Week10/learningreact/src/App.jsx
@@ -6,7 +6,9 @@ import './App.css'
 function App() {
   return (
       <div>
-        {/* <h1>hi there</h1> */}
+          <LinkedinPost />
+          <LinkedinPost />
+          <LinkedinPost />
           <LinkedinPost />
       </div>
   )
@@ -14,17 +16,45 @@ function App() {
 
 function LinkedinPost() {
   return (
-      <div style={{backgroundColor: 'gray',height: '100vh', padding: 10, justifyItems: 'center'}}>
-        <div style={{display: 'flex', height: 200, backgroundColor:  'white', width: 500, borderRadius: 20, marginLeft: 100}}>
-          <img style={{height: 100, width: 100, borderRadius: 200, objectFit: 'cover', marginLeft: 20, marginBottom: 20, marginTop: 20}} src="https://images.pexels.com/photos/2071882/pexels-photo-2071882.jpeg?cs=srgb&dl=pexels-wojciech-kumpicki-1084687-2071882.jpg&fm=jpg" alt="hello ji" />
-          <div style={{marginLeft: 20, height: '100%'}}>
-            <h1>Kunal Doliya</h1>
-            <h1 style={{fontSize: 10}}>Upcoming SDE Intern at Amazon</h1>
-            <h1>2h</h1>
+    <div style={{ backgroundColor: '#dff9fb', padding: 10, display: 'flex', justifyContent: 'center' }}>
+      <div style={{ backgroundColor: 'white', width: 500, borderRadius: 20, padding: 20 }}>
+        
+        <div style={{ display: 'flex', alignItems: 'center' }}>
+          <img
+            style={{
+              height: 100,
+              width: 100,
+              borderRadius: '50%',
+              objectFit: 'cover',
+              marginRight: 20,
+            }}
+            src="https://images.pexels.com/photos/2071882/pexels-photo-2071882.jpeg?cs=srgb&dl=pexels-wojciech-kumpicki-1084687-2071882.jpg&fm=jpg"
+            alt="hello ji"
+          />
+          
+          <div>
+            <h1 style={{ margin: 0 }}>Kunal Doliya</h1>
+            <p style={{ fontSize: 14, margin: 0 }}>Upcoming SDE Intern at Amazon</p>
+            <p style={{ fontSize: 14, margin: 0 }}>2h</p>
           </div>
         </div>
+        
+        <div style={{ marginTop: 20 }}>
+          <p>
+            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been
+            the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley
+            of type and scrambled it to make a type specimen book. It has survived not only five centuries,
+            but also the leap into electronic typesetting, remaining essentially unchanged. It was
+            popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages,
+            and more recently with desktop publishing software like Aldus PageMaker including versions of
+            Lorem Ipsum.
+          </p>
+        </div>
+
       </div>
-  )
+    </div>
+  );
 }
+
 
 export default App
