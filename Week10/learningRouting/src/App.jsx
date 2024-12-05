06c74e303fd7c94589b3da7f08ca0c77fdccbc83
kunaldoliya90 <kunal.doliya90@gmail.com>
2024-12-05 14:30:02 +0530

    last updates of learning routing

--- Week10/learningRouting/src/App.jsx
+++ Week10/learningRouting/src/App.jsx
@@ -1,9 +1,31 @@
 import './App.css'
-
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 function App() {
 
-  return (<div>hi there</div>
+  return (<div>
+    Hello Ji
+    <BrowserRouter>
+      <NavBar />
+      <Routes>
+        <Route path='/' element={<Landing />} />
+        <Route path='/neet/coaching-class11' element={<Class11Program />} />
+        <Route path='/neet/coaching-class12' element={<Class12Program />} />
+      </Routes>
+    </BrowserRouter>
+  </div>
   )
+}
+function Landing() {
+  return <div>Landing Page</div>
+}
+function Class11Program() {
+  return <div>class 11 component</div>
+}
+function Class12Program() {
+  return <div>class 12 component</div>
+}
+function NavBar() {
+  return <div>NavBar Component</div>
 }
 
 export default App
