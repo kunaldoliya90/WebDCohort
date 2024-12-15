9f43149f3c02d7fb4f3288e70c681f12f5f09e80
kunaldoliya90 <kunal.doliya90@gmail.com>
2024-12-15 19:22:07 +0530

    learning types in TS

--- Week-14/src/interfaces_types.ts
+++ Week-14/src/interfaces_types.ts
@@ -123,4 +123,96 @@
 
 // ------------------------------------------------------------------------------------------------
 
-// Types : 
+
+
+// interface People {
+//     name: string,
+//     age: number,
+//     // greet: () => string // One way to write function
+//     // greet2(): string
+// }
+
+// // create an object called person : People (people type)
+// let person: People = {
+//     name: "Kunal",
+//     age: 21,
+//     // greet: () => {
+//     //     return "Hi"
+//     // }
+// }
+
+// class Manager implements People {
+//     name: string;
+//     age: number;
+//     good: string
+
+//     constructor(name: string, age: number, good: string) {
+//         this.name = name;
+//         this.age = age;
+//         this.good = good
+//     }
+// }
+
+// let user = new Manager("Kunal", 21, "Yes");
+// console.log(user.name);
+// console.log(user.age);
+// console.log(user.good);
+
+// let greeting = person.greet();
+// console.log(greeting);
+
+// When I extends from a class I need to call super() of parent constructor in the childrens constructor
+// Learn Extends and Implenents in TS
+
+
+// ------------------------------------------------------------------------------------------------
+
+// Types
+
+// difference btween Interface and Types
+// we can create abstract classes
+// Type - lets aggregate data together
+// little syntax change of "=" in assigning
+
+// interface User {
+//     name: string;
+//     age: number;
+// }
+
+// type User2 = {
+//     name: string;
+//     age: number
+// }
+
+// function isLegal(user: User){
+//     return user.age >= 18
+// }
+
+// But we can't implement a type as we cn implement in interface
+// type -> let you do Unions and Intersection
+
+// Intersection
+
+type Employee = {
+    name: string;
+    startDate: string;
+
+}
+type Manager = {
+    name: string;
+    department: string;
+}
+type TeamLead = Employee & Manager; // I can also use "|"  or operator
+let e: Employee = {
+    name: "Kunal",
+    startDate: "01-02-2004",
+}
+let m: Manager = {
+    name: "Kunal",
+    department: "CS",
+}
+let t: TeamLead = {
+    name: "Kunal",
+    startDate: "01-02-2004",
+    department: "CS"
+}
